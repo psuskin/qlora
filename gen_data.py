d6c0c7320fe6c1fcb262cbc7a683a1ab6c46fb74
psuskin <philsuskin@gmail.com>
2023-08-09 20:59:34 +0200

    Setup for autoregressive training

--- gen_data.py
+++ gen_data.py
@@ -1,19 +1,56 @@
 import json
+import re
+
+_RE_COMBINE_WHITESPACE = re.compile(r"\s+")
+_RE_COMBINE_PERIOD = re.compile(r"\.+")
+
+def joinBlock(name, description):
+    if not name or name.isspace() or not description or description.isspace():
+        return ""
+    else:
+        joinedBlock = f"{name.strip()}: {description.strip()}".strip()
+        if joinedBlock.endswith("."):
+            return joinedBlock + " "
+        else:
+            return joinedBlock + ". "
+
+def cleanSequence(sequence):
+    return _RE_COMBINE_PERIOD.sub(".", _RE_COMBINE_WHITESPACE.sub(" ", sequence)).strip()
 
 with open("en_articles.json") as f:
     articles = json.load(f)
 
-print(list(articles["articles"][0].keys()))
+jsonArray = []
 
-# with open("data/en_articles_text_module.json", "w", encoding="utf-8") as f:
-#     for article in articles["articles"]:
-#         description = article['text'].replace('"', '\\"').replace('\\\\"', '\\"')
-#         f.write(f"{{\"input\": \"{description}\", \"output\": \"{article['module']}\"}}\n")
+toCut = [
+    '(Please always mark the terms ClassiX®, CyberEnterprise®, InstantView® and AppsWarehouse® with the trademark reference\"®\")',
+    '(Please always mark the terms ClassiX®, CyberEnterprise®, InstantView® and AppsWarehouse® with trademark reference\"®\")',
+    '...',
+]
+def cutDescription(description):
+    for cutStr in toCut:
+        description = description.replace(cutStr, "")
 
-with open("data/en_articles_text_module.tsv", "w", encoding="utf-8") as f:
-    f.write("input\toutput\n")
-    for article in articles["articles"]:
-        description = article['text'].replace('"', '\\"').replace('\\\\"', '\\"').replace('\n', ' ')
-        if '\t' in description:
-            continue
-        f.write(f"{description}\t{article['module']}\n")
+    return description.strip()
+
+for article in articles["articles"]:
+    if (not article["name"] or article["name"].isspace()) or len(article["blocks"]) < 1:
+        continue
+
+    sequence = article["name"]
+    description = cutDescription(article["description"]["text"])
+    if description and not description.isspace():
+        sequence += ": " + description
+    if sequence.endswith("."):
+        sequence += " "
+    else:
+        sequence += ". "
+    for block in article["blocks"]:
+        sequence += joinBlock(block["name"], block["description"]["text"])
+
+    sequence = cleanSequence(sequence)
+
+    jsonArray.append({"input": "", "output": sequence, "module": article["url"]})
+
+with open("data/en_articles_autoregressive.json", "w", encoding="utf-8") as f:
+    json.dump(jsonArray, f, ensure_ascii=False, indent=4)
---
+++ inference.py
@@ -0,0 +1,57 @@
+import os
+from os.path import exists, join, isdir
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, GenerationConfig
+from peft import PeftModel
+from peft.tuners.lora import LoraLayer
+
+# TODO: Update variables
+max_new_tokens = 64
+top_p = 0.9
+temperature=0.7
+
+# Base model
+model_name_or_path = 'huggyllama/llama-7b'
+# Adapter name on HF hub or local checkpoint path.
+adapter_path = 'output/guanaco-7b/checkpoint-1875/adapter_model'
+
+tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
+# Fixing some of the early LLaMA HF conversion issues.
+tokenizer.bos_token_id = 1
+
+# Load the model (use bf16 for faster inference)
+model = AutoModelForCausalLM.from_pretrained(
+    model_name_or_path,
+    torch_dtype=torch.bfloat16,
+    device_map={"": 0},
+    load_in_4bit=True,
+    quantization_config=BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_compute_dtype=torch.bfloat16,
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_quant_type='nf4',
+    )
+)
+
+model = PeftModel.from_pretrained(model, adapter_path)
+model.eval()
+
+def generate(model, prompt, max_new_tokens=max_new_tokens, top_p=top_p, temperature=temperature):
+    inputs = tokenizer(prompt, return_tensors="pt").to('cuda')
+
+    outputs = model.generate(
+        **inputs, 
+        generation_config=GenerationConfig(
+            do_sample=True,
+            max_new_tokens=max_new_tokens,
+            top_p=top_p,
+            temperature=temperature,
+        )
+    )
+
+    text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    print("Network output: ", text)
+    return text
+
+while (prompt := input("Enter prompt: ")) != "exit":
+    generate(model, prompt)
