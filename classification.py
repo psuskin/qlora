807f41a8e244f5217049f43927dd4395a1ff6e2f
psuskin <philsuskin@gmail.com>
2023-12-25 18:45:19 +0100

    Preparation for self-instruct classification

--- classification.py
+++ classification.py
@@ -1,10 +1,72 @@
+# Finetuning
 import numpy as np
 from datasets import Dataset
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, TrainingArguments, Trainer
-
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 
+# Instruct
+import torch
+from transformers import AutoModelForCausalLM, BitsAndBytesConfig, GenerationConfig
+
+# Inference
+from transformers import pipeline
+
 model_checkpoint = "distilbert-base-uncased"
+
+def instruct():
+    instructModel = "meta-llama/Llama-2-7b-chat-hf"
+
+    tokenizer = AutoTokenizer.from_pretrained(instructModel)
+    # Fixing some of the early LLaMA HF conversion issues.
+    tokenizer.bos_token_id = 1
+
+    # Load the model (use bf16 for faster inference)
+    model = AutoModelForCausalLM.from_pretrained(
+        instructModel,
+        torch_dtype=torch.bfloat16,
+        device_map={"": 0},
+        load_in_4bit=True,
+        quantization_config=BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_compute_dtype=torch.bfloat16,
+            bnb_4bit_use_double_quant=True,
+            bnb_4bit_quant_type='nf4',
+        )
+    )
+
+    prompts = [
+        "I will now provide you with a description of a module. Please generate 2-3 prompts that query which module is being described.\n\nModule description: Display and listing of version and copyright information.",
+    ]
+    
+    for prompt in prompts:
+        # https://huggingface.co/blog/llama2
+        llamaPrompt = f"""<s>[INST] <<SYS>>
+You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
+
+If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.
+<</SYS>>
+
+{prompt} [/INST]
+        """
+
+        print(prompt)
+
+        inputs = tokenizer(llamaPrompt, return_tensors="pt").to('cuda')
+
+        outputs = model.generate(
+            **inputs, 
+            generation_config=GenerationConfig(
+                do_sample=True,
+                max_new_tokens=512,
+                top_p=1,
+                temperature=0.01,
+            )
+        )
+
+        text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+        response = text.split("[/INST]", 1)[1].strip()
+
+        print(response)
 
 def finetune():
     full_dataset = Dataset.from_json('data/en_articles_classification_int.json')
@@ -91,8 +153,6 @@ def finetuneNoEval():
 
     trainer.train()
 
-from transformers import pipeline
-
 def inference(modelName):
     model = AutoModelForSequenceClassification.from_pretrained(f"output/{modelName}")
     tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
@@ -114,8 +174,10 @@ def inference(modelName):
         print(classifier(prompt))
 
 if __name__ == '__main__':
+    instruct()
+
     #finetune()
     #finetuneNoEval()
 
     #inference("distilbert-base-uncased-classification/checkpoint-30000")
-    inference("distilbert-base-uncased-classification-noeval/checkpoint-30000")
+    #inference("distilbert-base-uncased-classification-noeval/checkpoint-30000")
