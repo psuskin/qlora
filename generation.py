a32da88a46fe301aef301a5e7904c637f2c08688
psuskin <philsuskin@gmail.com>
2024-04-09 08:43:10 +0200

    Script works, but encounters OutOfMemoryError. Trying to fix by freeing variables?

--- generation.py
+++ generation.py
@@ -6,7 +6,7 @@ import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig, GenerationConfig
 
 # Inference
-from transformers import PeftModel
+from peft import PeftModel
 
 EN = True
 
@@ -56,19 +56,19 @@ def instruct(promptsPerClass=10):
         
         if EN:
             prompt = f"""In the following, I will provide you with the description of a module. Please generate {promptsPerClass} numbered question-answer pairs of the format 1. Q: question A: answer, where the question queries some relevant aspect of the module's functionality and the answer provides an accurate and helpful response to the corresponding question.
-            
-            Here are the requirements:
-            - The questions should be based on the module description.
-            - The answers should be accurate and helpful.
-            - The answers should not contain any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content.
-            - The responses should be socially unbiased and positive in nature.
-            - If a question is not factually coherent, explain why instead of providing an incorrect answer.
-            - If you do not know the answer to a question, please do not provide false information.
-            - The questions should be varied and cover different aspects of the module's functionality.
-            - Either an imperative or interrogative form can be used for the questions.
-            - Try not to repeat the verb for the questions to maximize variety.
-            
-            Module description: {text}"""
+
+Here are the requirements:
+- The questions should be based on the module description.
+- The answers should be accurate and helpful.
+- The answers should not contain any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content.
+- The responses should be socially unbiased and positive in nature.
+- If a question is not factually coherent, explain why instead of providing an incorrect answer.
+- If you do not know the answer to a question, please do not provide false information.
+- The questions should be varied and cover different aspects of the module's functionality.
+- Either an imperative or interrogative form can be used for the questions.
+- Try not to repeat the verb for the questions to maximize variety.
+
+Module description: {text}"""
         else:
             prompt = f"Ich gebe dir im Folgenden eine Beschreibung eines einzelnen Moduls. Bitte generiere {promptsPerClass} Frage-Antwort Paare, bei denen die Frage einen relevanten Aspekt der Funktionalität des Moduls abfragt und die Antwort eine genaue und hilfreiche Reaktion zur entsprechenden Frage bietet.\n\nModulbeschreibung: {text}"
         prompts.append(prompt)
@@ -78,12 +78,12 @@ def instruct(promptsPerClass=10):
         if EN:
             # https://huggingface.co/blog/llama2
             llamaPrompt = f"""<s>[INST] <<SYS>>
-            You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
+You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
 
-            If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.
-            <</SYS>>
+If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.
+<</SYS>>
 
-            {prompt} [/INST]"""
+{prompt} [/INST]"""
         else:
             llamaPrompt = f"Du bist ein hilfreicher Assistent. USER: {prompt} ASSISTANT:"
 
@@ -103,18 +103,24 @@ def instruct(promptsPerClass=10):
         text = tokenizer.decode(outputs[0], skip_special_tokens=True)
         response = text.split("[/INST]" if EN else "ASSISTANT:", 1)[1].strip()
 
-        print(llamaPrompt, response)
+        #print(llamaPrompt, response)
+        with open("instructOutput.txt", "a", encoding="utf-8") as f:
+            f.write(llamaPrompt + "\n" + response + "\n\n")
         # exit()
+            
+        # Free memory to avoid OutOfMemoryError
+        del inputs
+        del outputs
 
-        pattern = re.compile(r'\d+\.\s(.+?)(?:\n|$)')
-        matches = pattern.findall(response)
-        for match in matches:
-            pair = match.split("A:")
-            if len(pair) == 2:
-                instructions.append({"input": "In the following is a query. Write an appropriate response.\n\n### Query: " + pair[0].replace("Q:", "").strip().strip("\"") + "\n\n###Response:", "output": pair[1].strip().strip("\"")})
+        #pattern = re.compile(r'\d+\.\s(.+?)(?:\n|$)')
+        #matches = pattern.findall(response)
+        #for match in matches:
+        #    pair = match.split("A:")
+        #    if len(pair) == 2:
+        #        instructions.append({"input": "In the following is a query. Write an appropriate response.\n\n### Query: " + pair[0].replace("Q:", "").strip().strip("\"") + "\n\n###Response:", "output": pair[1].strip().strip("\"")})
 
-    with open(f"data/{'en' if EN else 'de'}_articles_generation_instruct{promptsPerClass}.json", "w", encoding="utf-8") as f:
-        json.dump(instructions, f, ensure_ascii=False, indent=4)
+    #with open(f"data/{'en' if EN else 'de'}_articles_generation_instruct{promptsPerClass}.json", "w", encoding="utf-8") as f:
+    #    json.dump(instructions, f, ensure_ascii=False, indent=4)
 
 def inference(modelName, adapter_path):
     prompts = [
