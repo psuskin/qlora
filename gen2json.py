ed97d2c031b02a8cfc38299bbff6cae829dc7932
psuskin <philsuskin@gmail.com>
2024-04-12 19:53:11 +0200

    Setup for intial finetuning

---
+++ gen2json.py
@@ -0,0 +1,39 @@
+import json
+
+queries = []
+contexts = []
+responses = []
+
+with open("instructOutput2.txt", encoding="utf-8") as f:
+    text = f.readlines()
+
+currentResponse = ""
+for line in text:
+    if line.startswith("Query: "):
+        queries.append(line.replace("Query: ", ""))
+        if currentResponse:
+            responses.append(currentResponse[:-2])
+            currentResponse = ""
+    elif line.startswith("Context: "):
+        contexts.append(line.replace("Context: ", ""))
+    elif line.startswith("Response: "):
+        currentResponse += line.replace("Response: ", "")
+    else:
+        currentResponse += f"{line}"
+responses.append(currentResponse[:-2])
+
+print(len(queries), len(contexts), len(responses))
+
+samples = []
+samplesNoContext = []
+for query, context, response in zip(queries, contexts, responses):
+    samples.append({"input": f"Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.\n\n### Instruction:\n{query}\n\n### Input:\n{context}\n\n### Response:",
+                    "output": response})
+    samplesNoContext.append({"input": f"Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.\n\n### Instruction:\n{query}\n\n### Input:\n\n### Response:",
+                             "output": response})
+
+with open("data/en_articles_klio_alpaca.json", "w", encoding="utf-8") as f:
+    json.dump(samples, f, ensure_ascii=False, indent=4)
+
+with open("data/en_articles_klio_alpaca_nocontext.json", "w", encoding="utf-8") as f:
+    json.dump(samplesNoContext, f, ensure_ascii=False, indent=4)
